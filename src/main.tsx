9222ac4f8fa5356b03a1ba2e4352073b3ac3deb6
agoy1602-coder <agoy1602@gmail.com>
2026-09-01 11:23:20 +0800

    Install Pocket TTS bridge at app startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import './services/pocketTtsBridge';
+import { installPocketTtsBridge } from './services/pocketTtsBridge';
 import App from './App.tsx';
 import './index.css';
+
+installPocketTtsBridge();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
