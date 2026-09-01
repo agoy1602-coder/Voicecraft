3e6a0ac66db88ade25c49c9970fa92e6a7a2b648
agoy1602-coder <agoy1602@gmail.com>
2026-09-02 04:35:42 +0800

    feat: register offline service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import {registerSW} from 'virtual:pwa-register';
 import { installPocketTtsBridge } from './services/pocketTtsBridge';
 import App from './App.tsx';
 import './index.css';
 
+registerSW({immediate: true});
 installPocketTtsBridge();
 
 createRoot(document.getElementById('root')!).render(
