d15d8b7e73e76573b08b9acd9117c198cf028a2b
agoy1602-coder <agoy1602@gmail.com>
2026-09-03 07:30:54 +0800

    diagnostic: trace offline bootstrap ordering

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,25 @@ import {registerSW} from 'virtual:pwa-register';
 import { installPocketTtsBridge } from './services/pocketTtsBridge';
 import App from './App.tsx';
 import './index.css';
+import { markVoiceCraftDiagnostic } from './diagnostics/offlineFreezeDiagnostic';
 
+markVoiceCraftDiagnostic('MAIN_MODULE_LOADED');
+
+markVoiceCraftDiagnostic('SW_REGISTER_BEGIN', {
+  online: navigator.onLine,
+  controller: Boolean(navigator.serviceWorker?.controller),
+});
 registerSW({immediate: true});
-installPocketTtsBridge();
+markVoiceCraftDiagnostic('SW_REGISTER_RETURNED');
 
+markVoiceCraftDiagnostic('POCKET_BRIDGE_INSTALL_BEGIN');
+installPocketTtsBridge();
+markVoiceCraftDiagnostic('POCKET_BRIDGE_INSTALL_RETURNED');
+
+markVoiceCraftDiagnostic('REACT_RENDER_BEGIN');
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+markVoiceCraftDiagnostic('REACT_RENDER_RETURNED');
